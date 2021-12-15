c8a05b5ae05b513ff5c9ef9eaebb74f8d182b19f
nabatee <alkhars.mo@gmail.com>
2021-12-15 09:05:08 -0800

    index

---
+++ 07-Pig-Game/dec 15/starter/script.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const diceEl = document.querySelector('.dice');
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+// BUTTONS
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+
+let activePlayer, currentScore, score, playing;
+
+const init = function () {
+  score = [0, 0];
+
+  playing = true;
+  currentScore = 0;
+  activePlayer = 0;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  diceEl.classList.add('hidden');
+};
+
+const swithPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+init();
+btnRoll.addEventListener('click', function () {
+  diceEl.classList.remove('hidden');
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    document.querySelector('img').src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+
+      console.log(dice);
+    } else {
+      swithPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    score[activePlayer] += currentScore;
+    if (score[activePlayer] < 20) {
+      document.getElementById(`score--${activePlayer}`).textContent =
+        score[activePlayer];
+      swithPlayer();
+    } else if (score[activePlayer] >= 20) {
+      playing = false;
+      document.getElementById(`score--${activePlayer}`).textContent =
+        score[activePlayer];
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+    }
+
+    console.log('hold', score);
+  }
+});
+
+btnNew.addEventListener('click', init);
