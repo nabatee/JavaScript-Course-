faed5ec1425d6b43e014539c11f402731d422433
nabatee <alkhars.mo@gmail.com>
2021-12-21 16:24:28 -0800

    index

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,278 @@
 'use strict';
+
+// // selecting elements
+// const player0El = document.querySelector('.player--0');
+// const player1El = document.querySelector('.player--1');
+// const score0El = document.querySelector('#score--0');
+// const score1El = document.getElementById('score--1');
+// const current0El = document.getElementById('current--0');
+// const curent1El = document.getElementById('current--1');
+// const diceEl = document.querySelector('.dice');
+// const btnNew = document.querySelector('.btn--new');
+// const btnRoll = document.querySelector('.btn--roll');
+// const btnHold = document.querySelector('.btn--hold');
+
+// let scores = [0, 0];
+// let currentScore = 0;
+// let activePlayer = 0;
+// let playing = true;
+
+// // starting condition
+
+// const init = function () {
+//   scores = [0, 0];
+//   currentScore = 0;
+//   activePlayer = 0;
+//   playing = true;
+
+//   score0El.textContent = 0;
+//   score1El.textContent = 0;
+//   current0El.textContent = 0;
+//   curent1El.textContent = 0;
+
+//   diceEl.classList.add('hidden');
+//   player0El.classList.remove('player--winner');
+//   player1El.classList.remove('player--winner');
+//   player0El.classList.add('player--active');
+//   player1El.classList.remove('player--active');
+// };
+
+// init();
+// const switchPlayer = function () {
+//   document.getElementById(`current--${activePlayer}`).textContent = 0;
+//   currentScore = 0;
+//   activePlayer = activePlayer === 0 ? 1 : 0;
+//   player0El.classList.toggle('player--active');
+//   player1El.classList.toggle('player--active');
+// };
+
+// //rolling dice functionalitty
+// btnRoll.addEventListener('click', function () {
+//   if (playing) {
+//     // 1. generate a random dice roll
+//     const dice = Math.trunc(Math.random() * 6) + 1;
+//     console.log(dice);
+
+//     // 2. display dice
+
+//     diceEl.classList.remove('hidden');
+//     diceEl.src = `dice-${dice}.png`;
+//     // 3. check foor rolled 1: if true
+
+//     if (dice !== 1) {
+//       // add dice to current score
+//       currentScore += dice;
+//       document.getElementById(`current--${activePlayer}`).textContent =
+//         currentScore;
+//     } else {
+//       //switch to next player
+//       switchPlayer();
+//     }
+//   }
+// });
+
+// btnHold.addEventListener('click', function () {
+//   // 1 - add current score to active player's score
+//   if (playing) {
+//     scores[activePlayer] += currentScore; //   < means -> scores[1] = scores[1] + currentScore
+//     console.log(scores[activePlayer]);
+//     document.getElementById(`score--${activePlayer}`).textContent =
+//       scores[activePlayer];
+
+//     //2 - check if player's score is >= 100
+//     if (scores[activePlayer] >= 20) {
+//       // finish he game
+//       playing = false;
+//       diceEl.classList.add('hidden');
+//       document
+//         .querySelector(`.player--${activePlayer}`)
+//         .classList.add('player--winner');
+//       document
+//         .querySelector(`.player--${activePlayer}`)
+//         .classList.remove('.player--active');
+//     } else {
+//       // if ot switch to the next play
+//       switchPlayer();
+//     }
+//   }
+// });
+
+// btnNew.addEventListener('click', init);
+
+let score = document.querySelector('.score');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+// BUTTONS
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+
+//idk
+
+//starting
+
+score = [0, 0];
+score[0] = 0;
+
+let activeScore = 0;
+let currentScore = 0;
+let activePlayer = 0;
+let currentPlayer = 0;
+let playing = true;
+
+const gameInit = function () {
+  document.querySelector('img').classList.add('hidden');
+  score = [0, 0];
+  score = 0;
+  btnRoll.disabled = false;
+  btnHold.disabled = false;
+  currentScore = 0;
+  activeScore = 0;
+  currentPlayer = 0;
+  score0El.textContent = score;
+  score1El.textContent = score;
+  current1El.textContent = activeScore;
+  current0El.textContent = `${currentScore}`;
+  player1El.classList.remove('player--winner');
+  player0El.classList.remove('player--winner');
+};
+
+function disableBttns() {
+  if (playing === false) {
+    btnRoll.disabled = true;
+    btnHold.disabled = true;
+  } else if (playing === true) {
+    btnRoll.disabled = false;
+    btnHold.disabled = false;
+  }
+}
+
+const winningG = function () {
+  if (activePlayer) {
+    score = activeScore;
+  } else if (currentPlayer) {
+    score = currentScore;
+  } // need to add current :/
+  if (score < 20) {
+    if (activePlayer) {
+      score0El.textContent += current0El.textContent;
+      // current0El.textContent = score;
+    } else if (currentPlayer) {
+      score1El.textContent += current1El.textContent;
+      // current1El.textContent = score;
+    }
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    score0El.textContent = current0El.textContent;
+    score1El.textContent = current1El.textContent;
+  } else if (score >= 20) {
+    btnRoll.disabled = true;
+    btnHold.disabled = true;
+    if (activePlayer) {
+      player0El.classList.add('player--winner');
+      score0El.textContent = current0El.textContent;
+    } else if (currentPlayer) {
+      player1El.classList.add('player--winner');
+      score1El.textContent = current1El.textContent;
+    }
+  }
+};
+gameInit();
+
+btnRoll.addEventListener('click', function () {
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  /*  current0El.textContent = currentScore; */
+  document.querySelector('img').classList.remove('hidden');
+  document.querySelector('img').src = `dice-${dice}.png`;
+  if (activePlayer) {
+    score = activeScore;
+  } else if (currentPlayer) {
+    score = currentScore;
+  } //
+  activePlayer = player0El.classList.contains('player--active');
+  currentPlayer = player1El.classList.contains('player--active');
+  if (dice > 1) {
+    /*     score = currentScore; */
+
+    if (activePlayer) {
+      // current0El.textContent = currentScore;
+      activeScore += dice;
+      current0El.textContent = activeScore;
+    } else if (currentPlayer) {
+      currentScore += dice;
+      current1El.textContent = currentScore;
+    }
+  } else if (dice === 1 && activePlayer) {
+    activeScore = 0;
+    score0El.textContent = activeScore;
+    current0El.textContent = activeScore;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  } else if (dice === 1 && currentPlayer) {
+    currentScore = 0;
+    score1El.textContent = currentScore;
+    current1El.textContent = currentScore;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+  if (score >= 20) {
+    winningG();
+  }
+
+  /*     if (activePlayer) {
+      score0El.textContent = activeScore;
+    } else if (currentPlayer) {
+  
+    } */
+
+  console.log(
+    'DICE ---',
+    dice,
+    'activeScore---',
+    activeScore,
+    'currentScore----',
+    currentScore
+  );
+});
+
+btnHold.addEventListener('click', function () {
+  if (activePlayer) {
+    score = activeScore;
+  } else if (currentPlayer) {
+    score = currentScore;
+  } // need to add current :/
+
+  activePlayer = player0El.classList.contains('player--active');
+  currentPlayer = player1El.classList.contains('player--active');
+  if (score < 20) {
+    if (activePlayer) {
+      score0El.textContent += current0El.textContent;
+      // current0El.textContent = score;
+    } else if (currentPlayer) {
+      score1El.textContent += current1El.textContent;
+      // current1El.textContent = score;
+    }
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+    score0El.textContent = current0El.textContent;
+    score1El.textContent = current1El.textContent;
+  } else if (score >= 20) {
+    btnRoll.disabled = true;
+    btnHold.disabled = true;
+    if (activePlayer) {
+      player0El.classList.add('player--winner');
+      score0El.textContent = current0El.textContent;
+    } else if (currentPlayer) {
+      player1El.classList.add('player--winner');
+      score1El.textContent = current1El.textContent;
+    }
+  }
+});
+btnNew.addEventListener('click', gameInit);
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,178 @@
 'use strict';
+/* console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAge(1991);
+
+console.log('///////');
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1994);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge; // method borrowing
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
+ */
+
+/* const calcAge = function (birthYear) {
+  let age = 2030 - birthYear;
+  console.log(age);
+  console.log(this);
+};
+
+calcAge(1994);
+
+const calcAgeArrow = birthYear => {
+  let age = 2030 - birthYear;
+  console.log(age);
+  console.log(this);
+};
+
+calcAgeArrow(1994);
+
+let mohammad = {
+  year: 1994,
+  year1: 2002,
+  calcAge() {
+    console.log(2040 - this.year1);
+  },
+};
+
+mohammad.calcAge();
+
+let lulu = {
+  year1: 2020,
+};
+lulu.calcAge = mohammad.calcAge;
+
+lulu.calcAge();
+
+console.log(lulu.calcAge);
+ */
+// !!!!!!!this will create a local property in the global object!!!!!!!!
+/* var firstName = 'matilda'; */
+
+// jonas = { ... this is object literal. not a scope}
+/* const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () { */
+/*  console.log(this); */
+
+// solution one
+/*     console.log(2037 - this.year);
+    const self = this; // self or that
+    const isMillenial = function () {
+      console.log(self);
+      console.log(self.year >= 1081 && self.year <= 1996);
+    }; */
+
+/// NOOOOOTTTT PRACTICCCEEEEEDDDD
+// solution 2 ( USE AN ARROW FUNCNTION WITH THIS INHERITING THE PARENT SCOPE)
+/*     const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1081 && this.year <= 1996);
+    };
+
+    isMillenial();
+  },
+
+  // this will access the window object bc jonas isn't a function, it's a lateral object
+  greet: function () {
+    console.log(`hey ${this.firstName} `);
+  },
+};
+
+jonas.calcAge();
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+
+addExpr(2, 5);
+addExpr(2, 5, 5, 6);
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(1, 3);
+ */
+
+// PRIMITVE VS OBJECT
+/* let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+
+friend.age = 27;
+
+console.log('Friend:', friend);
+console.log('Jonas:', me);
+ */
+
+// primitive types
+let lastName = 'William';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// referemce types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage: ', marriedJessica);
+
+// copyinig objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Fatima'],
+};
+
+const JessicaCopy = Object.assign({}, jessica2);
+JessicaCopy.lastName = 'Davis';
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage: ', JessicaCopy);
+
+JessicaCopy.family.push('Mary');
+JessicaCopy.family.push('John');
+console.log('Before marriage:', jessica2);
+console.log('After marriage: ', JessicaCopy);
