a1493593cba8171721ee901f1555bf526ee336f0
nabatee <alkhars.mo@gmail.com>
2021-12-24 17:53:28 -0800

    index

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/* 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
@@ -29,3 +29,107 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+ */
+
+/* 1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1. Create one player array for each team (variables 'players1' and 'players2')
+
+// const [players1, players2] = game.players
+const players1 = [...game.players[0]]; // Bayern Munich
+const players2 = [...game.players[1]]; //Borrussia Dortmund
+console.log(players1);
+console.log(players2);
+//2. or Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+const [gk, ...fieldPlayers] = players1;
+console.log(gk);
+console.log(fieldPlayers);
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+//4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+const playersFinal = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(playersFinal);
+//5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1);
+console.log(draw);
+console.log(team2);
+//6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+//TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+const printGoals = function (...names) {
+  let goals = 0;
+
+  goals = names.length;
+
+  console.log(`NAMES are ${names}, and GOALS are ${goals}`);
+};
+/* const printGoals = function (...names) {
+  let goals = 0;
+  for (let i = 0; i < names.length; i++) {
+    goals = names.length;
+  }
+  console.log(
+    `the player names are ${names} and the number of goals scored in total is ${goals}`
+  );
+}; */
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+team1 < team2 && console.log('team1 likely to win is team1');
